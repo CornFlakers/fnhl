28551ce091155a8ab2e9ecf2017cf7cf8b31482b
Skylar <wsbarth92@gmail.com>
2022-05-27 03:11:43 -0400

    fix: styled popup

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Popup = (props) => {
   return (props.trigger) ? (
-    <div id="popup" className='pointer-events-auto fixed z-50 top-28 left-2 right-2 bg-gray-500 flex justify-center items-center'>
-        <div id="popup-inner" className='pointer-events-auto relative p-[8px] w-full max-w-xl'>
+    <div id="popup" className='fixed z-50 left-0 top-28 w-[360px] max-w-[360px] bg-gray-500 flex justify-center items-center'>
+        <div id="popup-inner" className='relative p-[8px] w-full max-w-xl'>
             <button id="close-btn" className='absolute hover:bg-blue-500 top-[8px] right-[10px] text-white' onClick={() => props.setTrigger(false)}>Close</button>
             {props.children}
         </div>
